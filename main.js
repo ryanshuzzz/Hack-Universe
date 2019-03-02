a492ba94cfc454fb4572ee5e988b96b16c945fdb
Ryan <ryanshu78@gmail.com>
2019-03-02 14:53:59 -0800

    switch to p5

--- main.js
+++ main.js
@@ -1,26 +1,7 @@
-const config = {
-	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
-	backgroundColor: "#222222",
-	parent: "game-canvas",
-	scene: {
-		preload: preload,
-		create: create,
-		update: update
-	}
-};
-
-const game = new Phaser.Game(config);
-
-function preload() {
-  // Preload Resources
-}
-
-function create() {
-	//Init 
-}
-
-function update(time, delta) {
-	//Update
-}
+function setup() {
+    createCanvas(1280, 720);
+  }
+  
+  function draw() {
+    fill(255);
+  }
