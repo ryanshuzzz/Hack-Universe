883a57864a72c7e23ab4bafdc74e6306f8d07d95
Ryan <ryanshu78@gmail.com>
2019-03-02 23:34:42 -0800

    update universe

--- universe.js
+++ universe.js
@@ -1,12 +1,6 @@
 class universe{
 	// Init spawn solar systems
-	spawn(){
-		var i;
-		for(i = 0; i < 1; i++){
-			this.solar_systems.push(new solarsystem(0, 0, 1920, 1080));
-			//this.solar_systems.push(new solarsystem(Math.floor(Math.random() * size), Math.floor(Math.random() * size), size, size));
-		}
-	}
+
 	
 	constructor(){
 		this.cam_x = 0;
@@ -14,6 +8,9 @@ class universe{
 		this.cam_speed = 30;
 		
 		this.solar_systems = [];
+
+		this.solarsystemspawnx;
+		this.solarsystemspawny;
 		
 		this.spawn();
 	}
@@ -23,8 +20,14 @@ class universe{
 	}
 	
 	// Check in vicinity of player to see if solar system should be spawned
-	move_spawn(){
-		//
+
+	spawn(){
+		var i;
+		for(i = 0; i < 3; i++){
+			this.solar_systems.push(new solarsystem(0, 0, 50, 50));
+
+			//this.solar_systems.push(new solarsystem(Math.floor(Math.random() * size), Math.floor(Math.random() * size), size, size));
+		}
 	}
 	
 	// Move the camera
@@ -45,7 +48,7 @@ class universe{
 			
 			var dist = this.distance(this.cam_x - canvas.width/2, this.cam_y - canvas.height/2, sol_x, sol_y);
 			console.log(Math.abs(dist) + ", "+ canvas.width);
-			if(Math.abs(dist) > canvas.width){
+			if(Math.abs(dist) < canvas.width + rad){
 				this.solar_systems[i].set_pos(this.cam_x, this.cam_y);
 				this.solar_systems[i].rotatePlanets();
 				this.solar_systems[i].drawPlanets();
