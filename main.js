fd262062ff546f1d166830d958d26c7828771bb6
Jimmy Kwan <jkwan3@sfs-wifi-dhcp-10-143-215-150.wifi.sfsu.edu>
2019-03-02 22:51:13 -0800

    main

--- main.js
+++ main.js
@@ -7,22 +7,22 @@ function draw() {
 	
 	if (keyIsDown(87)){
 		uni.move(0, 1);
-		stars.setDelta(0,1)
+		stars.setDelta(0,5)
 	}
 	// Down
 	if (keyIsDown(83)){
 		uni.move(0, -1);
-		stars.setDelta(0,-1);
+		stars.setDelta(0,-5);
 	}
 	// Left
 	if (keyIsDown(68)){
 		uni.move(-1, 0);
-		stars.setDelta(-1,0);
+		stars.setDelta(-5,0);
 	}
 	// Right
 	if (keyIsDown(65)){
 		uni.move(1, 0);
-		stars.setDelta(1,0);
+		stars.setDelta(5,0);
 	}
 	
 	background(0);
